c40b4e8857b1703d1f5fa2102f504e4d0a0ea185
olearczuk <olearczuk.szymon@gmail.com>
2021-01-28 02:23:54 +0100

    Modified SectorsManagerImplementation so it does not have 10% overhead

--- public-tests/tests/sectors_manager.rs
+++ public-tests/tests/sectors_manager.rs
@@ -20,20 +20,66 @@ async fn read_before_write() {
 async fn drive_can_store_data() {
     // given
     let root_drive_dir = tempdir().unwrap();
-    let sectors_manager = build_sectors_manager(root_drive_dir.into_path());
+    let path = root_drive_dir.into_path();
+    let sectors_manager = build_sectors_manager(path);
 
     // when
-    for i in 1..100 {
+    for i in 1..350 {
         sectors_manager
             .write(i, &(SectorVec(vec![2; 4096]), i, 1))
             .await;
     }
 
     // then
-    for i in 1..100 {
+    for i in 1..350 {
         let data = sectors_manager.read_data(i).await;
         assert_eq!(data.0.len(), 4096);
         assert_eq!(data.0, vec![2; 4096]);
         assert_eq!(sectors_manager.read_metadata(i).await, (i, 1));
+    }
+}
+
+#[tokio::test]
+async fn drive_can_restore_data() {
+    // given
+    let root_drive_dir = tempdir().unwrap();
+    let path = root_drive_dir.into_path();
+    {
+        let sectors_manager = build_sectors_manager(path.clone());
+        // when
+        for i in 1..20 {
+            sectors_manager
+                .write(i, &(SectorVec(vec![2; 4096]), i, 1))
+                .await;
+        }
+    }
+
+    // next sectors manager
+    let sectors_manager2 = build_sectors_manager(path.clone());
+
+
+    // when
+    for i in 10..30 {
+        sectors_manager2
+            .write(i, &(SectorVec(vec![3; 4096]), i + 10, 2))
+            .await;
+    }
+
+    // then
+    // old values
+    for i in 1..10 {
+        let data = sectors_manager2.read_data(i).await;
+        assert_eq!(data.0.len(), 4096);
+        assert_eq!(data.0, vec![2; 4096]);
+        assert_eq!(sectors_manager2.read_metadata(i).await, (i, 1));
+    }
+
+    // assert_eq!(1, 2);
+    // changed values
+    for i in 10..30 {
+        let data = sectors_manager2.read_data(i).await;
+        assert_eq!(data.0.len(), 4096);
+        assert_eq!(data.0, vec![3; 4096]);
+        assert_eq!(sectors_manager2.read_metadata(i).await, (i + 10, 2));
     }
 }
--- solution/src/sectors_manager.rs
+++ solution/src/sectors_manager.rs
@@ -1,57 +1,151 @@
 pub mod sectors_manager {
-    use crate::stable_storage::stable_storage::{StableStorageImplementation, read, write};
-    use crate::{SectorIdx, SectorVec, SectorsManager, StableStorage, SECTOR_VEC_SIZE};
+    use crate::stable_storage::stable_storage::{read, write};
+    use crate::{SectorIdx, SectorVec, SectorsManager, SECTOR_VEC_SIZE};
     use std::path::PathBuf;
-    use std::sync::Arc;
-    use std::fs::create_dir_all;
+    use std::fs::{create_dir_all};
     use std::convert::TryInto;
+    use std::collections::HashMap;
+    use std::sync::Arc;
+    use tokio::sync::Mutex;
 
     pub struct SectorsManagerImplementation {
         path: PathBuf,
+        metadata_path: PathBuf,
+        /// metadata_map keeps filename and offset of given sector
+        metadata_info_map: Arc<Mutex<HashMap<SectorIdx, (String, usize)>>>,
+        /// total size of stored metadata (used for computing file and offset in the file)
+        metadata_total_size: Arc<Mutex<usize>>,
     }
 
-    const METADATA_SIZE: usize = 8 + 1;
+    const METADATA_SIZE: usize = 8 + 8 + 1;
+    const METADATA: &str = "metadata";
+    const METADATAS_PER_FILE: usize = SECTOR_VEC_SIZE / METADATA_SIZE;
+    const METADATAS_FILE_SIZE: usize = METADATAS_PER_FILE * METADATA_SIZE;
+
+    fn get_metadata_info(vec: &Vec<u8>, offset: usize) -> (SectorIdx, u64, u8) {
+        let sector_idx_vec_res: Result<[u8; 8], _> = vec[offset..offset + 8].try_into();
+        let sector_idx_vec = sector_idx_vec_res.unwrap();
+        let sector_idx = u64::from_be_bytes(sector_idx_vec);
+
+        let timestamp_vec_res: Result<[u8; 8], _> = vec[offset + 8..offset + 16].try_into();
+        let timestamp_vec = timestamp_vec_res.unwrap();
+        let timestamp = u64::from_be_bytes(timestamp_vec);
+
+        let write_rank = vec[offset + METADATA_SIZE - 1];
+        (sector_idx, timestamp, write_rank)
+    }
+
 
     impl SectorsManagerImplementation {
         pub fn new(path: PathBuf) -> Arc<dyn SectorsManager> {
+            let metadata_path = path.clone().join(METADATA);
             create_dir_all(&path).unwrap();
-            let manager = SectorsManagerImplementation { path };
+            create_dir_all(&metadata_path).unwrap();
 
+            let mut file_number = 0;
+            let mut metadata_total_size = 0;
+            let mut metadata_info_map = HashMap::new();
+            loop {
+                let file_path = metadata_path.join(file_number.to_string());
+                let read_res = std::fs::read(file_path);
+                if read_res.is_err() {
+                    break;
+                }
+                let vec = read_res.unwrap();
+                metadata_total_size += vec.len();
+                for offset in (0..vec.len()).step_by(METADATA_SIZE) {
+                    let (sector_idx, _, _) = get_metadata_info(&vec, offset);
+                    metadata_info_map.insert(sector_idx, (file_number.to_string(), offset));
+                }
+                file_number += 1;
+            }
+
+            let manager = SectorsManagerImplementation {
+                path,
+                metadata_path,
+                metadata_info_map: Arc::new(Mutex::new(metadata_info_map)),
+                metadata_total_size: Arc::new(Mutex::new(metadata_total_size)),
+            };
             Arc::new(manager)
         }
     }
 
-    // TODO: avoid > 10% overhead
     #[async_trait::async_trait]
     impl SectorsManager for SectorsManagerImplementation {
         async fn read_data(&self, idx: SectorIdx) -> SectorVec {
-            match read(&self.path, &idx.to_string()).await {
+            let metadata_offset_map = self.metadata_info_map.lock().await;
+            match metadata_offset_map.get(&idx) {
                 None => SectorVec::new(),
-                Some(vec) => SectorVec(vec[METADATA_SIZE..].to_vec()),
+                Some(_) => {
+                    match read(&self.path, &idx.to_string()).await {
+                        None => SectorVec::new(),
+                        Some(vec) => SectorVec(vec),
+                    }
+                },
             }
         }
 
         async fn read_metadata(&self, idx: SectorIdx) -> (u64, u8) {
-            match read(&self.path, &idx.to_string()).await {
+            let metadata_offset_map = self.metadata_info_map.lock().await;
+            match metadata_offset_map.get(&idx) {
                 None => (0, 0),
-                Some(vec) => {
-                    let write_rank = vec[METADATA_SIZE - 1];
-                    let timestamp_vec_res: Result<[u8; 8], _> = vec[..METADATA_SIZE - 1].try_into();
-                    let timestamp_vec = timestamp_vec_res.unwrap();
-                    let timestamp = u64::from_be_bytes(timestamp_vec);
-                    (timestamp, write_rank)
-                },
+                Some((filename, offset)) => {
+                    match read(&self.metadata_path, filename).await {
+                        None => (0, 0),
+                        Some(vec) => {
+                            let (_, timestamp, write_rank) = get_metadata_info(&vec, *offset);
+                            (timestamp, write_rank)
+                        },
+                    }
+                }
             }
         }
 
         async fn write(&self, idx: SectorIdx, sector: &(SectorVec, u64, u8)) {
             let (data, timestamp, write_rank) = sector;
-            let mut metadata_vec = timestamp.to_be_bytes().to_vec();
-            metadata_vec.push(*write_rank);
 
-            let mut vec = data.0.clone();
-            metadata_vec.append(&mut vec);
-            write(&self.path, &idx.to_string(), metadata_vec.as_slice()).await.unwrap();
+            if write(&self.path, &idx.to_string(), data.0.as_slice()).await.is_err() {
+                return
+            }
+
+            let mut metadata_vec = idx.to_be_bytes().to_vec();
+            let mut timestamp_vec = timestamp.to_be_bytes().to_vec();
+            metadata_vec.append(&mut timestamp_vec);
+            metadata_vec.push(*write_rank);
+            {
+                let mut metadata_info_map = self.metadata_info_map.lock().await;
+                let mut metadata_total_size = self.metadata_total_size.lock().await;
+
+                let (filename, offset) = match metadata_info_map.get(&idx) {
+                    None => {
+                        let file_number = *metadata_total_size / METADATAS_FILE_SIZE;
+                        let offset = *metadata_total_size % METADATAS_FILE_SIZE;
+                        (file_number.to_string(), offset)
+                    }
+                    Some((filename, offset)) => (filename.clone(), *offset)
+                };
+
+                let mut val = match read(&self.metadata_path, &filename).await {
+                    None => vec![],
+                    Some(vec) => vec,
+                };
+
+                if offset == val.len() {
+                    val.append(&mut metadata_vec);
+                } else {
+                    for i in offset..offset + METADATA_SIZE {
+                        val[i] = metadata_vec[i - offset];
+                    }
+                }
+
+                if write(&self.metadata_path, &filename, &val.as_slice()).await.is_err() {
+                    return
+                }
+                if !metadata_info_map.contains_key(&idx) {
+                    (*metadata_info_map).insert(idx, (filename, offset));
+                    *metadata_total_size += METADATA_SIZE;
+                }
+            }
 
         }
     }
